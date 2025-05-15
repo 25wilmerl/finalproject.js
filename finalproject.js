94ec6e36731cf00a04fec817a89606a36b272f9d
Wilmer <25WilmerL@badinhs.org>
2025-05-15 10:48:46 -0400

    i figured out how to get the number

--- finalproject.js
+++ finalproject.js
@@ -1,7 +1,27 @@
-let i = 0
+//Guess the number game:  The computer will randomly choose a number between 1-100.  
+//Prompt the user for a guess of what number the computer picked.  
+//The computer will respond with a clue as to whether the user's guess is too high, too low, or correct.  
+//Keep going until the user gets it right.  
+//When the user gets it right, tell them their score.  
+//Keep track of the users scores, and after each round, display their average number of guesses needed.  
 
-function numbers(max) {
+
+
+let i = 0
+let input = 0;
+function numbers(max){
     i = Math.ceil(Math.random() * max)
-    return i
+   console.log(i)
 }
-console.log(numbers(100))
+function game(){
+    input = prompt("pick a number 1-100")
+    numbers(100);
+while(i !== input){
+    if(input > i){
+        input = prompt("pfft! too large (it wont fit)")
+    } else{ if(input < i){
+        input = prompt("too small! try viagra")
+    }}}
+alert(`Good job!! The number was ${i}`)
+}
+game();
---
+++ rps.js
@@ -0,0 +1,72 @@
+const Rock = 1;
+const Paper = 2;
+const Scissors = 3;
+let Playerscore = 0;
+let Botscore = 0;
+let PlayerChoice = undefined;
+let BotChoice = undefined;
+let input = undefined;
+
+
+function Playerinput(){
+    input = prompt("Rock, Paper, Scissors");
+
+input = input.toLowerCase();
+    switch(input){
+        case "Rock":
+        case "rock":
+            PlayerChoice = 1;
+            break;
+        case "Paper":
+        case "paper":
+            PlayerChoice = 2;
+            break;
+        case "Scissors":
+        case "scissors":
+            PlayerChoice = 3;
+            break;
+        default:
+ function Botinput(max){
+        console.log("You tied, I hope you do better next time")
+    } else if(PlayerChoice == Rock){
+        if(BotChoice == Scissors){
+            PlayerChoice = Playerscore +1;
+            Playerscore = Playerscore +1;
+            console.log("The Bot picked scissors, you won:)");
+        } else {
+            Botscore = Botscore +1;
+ function Botinput(max){
+    }
+    else if(PlayerChoice == Paper){
+        if(BotChoice == Rock){
+            PlayerChoice = Playerscore +1;
+            Playerscore = Playerscore +1;
+            console.log("The Bot picked rock, you win ;)");
+        } else {
+            Botscore = Botscore +1;
+            console.log("You lost, the Bot chose rock, try again :(");
+        }
+    } else if(PlayerChoice == Scissors){
+        if(BotChoice == Paper){
+            PlayerChoice = Playerscore +1;
+            Playerscore = Playerscore +1;
+            console.log("The Bot picked rock, you win :)");
+        } else {
+            Botscore = Botscore +1;
+            console.log("You lost, the Bot chose paper, try again :(");
+        }
+    } console.log(Playerscore);
+    console.log(Botscore);
+    } console.log(`Playerscore is ${Playerscore}`);
+    console.log(`Botscore is ${Botscore}`);
+}
+
+while (Playerscore < 3 && Botscore < 3){
+while (Playerscore < 5 && Botscore < 5){
+    Botinput(3);
+}
+while (Botscore < 3 && Playerscore < 3){
+while (Botscore < 5 && Playerscore < 5){
+    Playerinput(3);
+}
+};
